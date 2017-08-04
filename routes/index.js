4d4709b0ff7cfa76fad02567cccdd86483215f02
HySu <colorlhs@gmail.com>
2017-08-04 11:43:59 +0900

    title commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tworaveler_server_dummy' });
 });
 
 module.exports = router;
